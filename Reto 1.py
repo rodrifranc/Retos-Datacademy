6b90cf36bb4a56bd91790130c4893597a106c732
Rodrigo Franco <rifs311@gmail.com>
2021-05-15 23:27:24 -0400

    Agregado calculo de tipo de triangulo, falta resolver errores

--- Reto 1.py
+++ Reto 1.py
@@ -1,5 +1,5 @@
 #Este programa calculará el área de un trangulo dados la base y la altura
-
+from math import sqrt
 print("Vamos a calcular el area de un triangulo, por favor ingrese los datos: ")
 
 
@@ -13,4 +13,22 @@ while base<=0 or altura<=0:
     altura=float(input("Ingrese la altura en metros: ")) 
 
 area=(base*altura)/2
-print(f"El area del triangulos es {area} metros cuadrados")
+print(f"El area del triangulo es {area} metros cuadrados")
+
+#Calcularemos si el triangulo es isósceles, equilatero o escaleno
+
+c1=(base/2)^2
+c2=altura^2
+def isosceles(self, a):
+    if c1+c2==a^2:
+        print("El triangulo es isosceles!!")
+    else:
+        print("El triangulo es Escaleno!!")
+
+
+if (base^2)==((base/2)^2)+(altura^2):
+    print("El triangulo es equilatero!!")
+else:
+    print("El triangulo no es equilatero, por favor ingrese la longitud de uno de los lados: \n ")
+    lado_1=float(input("Lado 1: "))
+    isosceles(lado_1)
