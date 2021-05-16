ef7dd23318b44287c5f2f1862e6797d7353aaffe
Rodrigo Franco <rifs311@gmail.com>
2021-05-16 13:06:42 -0400

    Corregido error intercambiado ^ por **

--- Reto 1.py
+++ Reto 1.py
@@ -17,16 +17,16 @@ print(f"El area del triangulo es {area} metros cuadrados")
 
 #Calcularemos si el triangulo es isósceles, equilatero o escaleno
 
-c1=(base/2)^2
-c2=altura^2
-def isosceles(self, a):
-    if c1+c2==a^2:
+c1=(base/2)**2
+c2=altura**2
+def isosceles(a):
+    if c1+c2==a**2:
         print("El triangulo es isosceles!!")
     else:
         print("El triangulo es Escaleno!!")
 
 
-if (base^2)==((base/2)^2)+(altura^2):
+if (base**2)==((base/2)**2)+(altura**2):
     print("El triangulo es equilatero!!")
 else:
     print("El triangulo no es equilatero, por favor ingrese la longitud de uno de los lados: \n ")
