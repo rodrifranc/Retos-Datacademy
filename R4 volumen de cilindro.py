daaae36e388fd661a0fe90da76bb85e0ea5b10c8
Rodrigo Franco <rifs311@gmail.com>
2021-05-17 14:52:15 -0400

    Primer codigo del reto 4 volumen de cilindro

---
+++ R4 volumen de cilindro.py
@@ -0,0 +1,13 @@
+#Vamos a calcular el volumen de un cilindro partiendo del radio de base y la altura
+from math import pi
+print("Este programa sirve para calcular el volumen de un cilindro")
+
+def run():
+    radio_base=float(input("Por favor ingresa el radio de la base del cilindro: "))
+    altura=float(input("Por favor ingresa la altura del cilindro: "))
+    volumen_cilindro=(pi*(radio_base**2)*altura)
+    return volumen_cilindro
+        
+
+if __name__=="__main__":
+    run()
