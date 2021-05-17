11ebdd54588d72e37032fe34f279583fee4b51bb
Rodrigo Franco <rifs311@gmail.com>
2021-05-17 14:27:26 -0400

    Version final de conversor de km a millas

--- R3 coversor de distancia.py
+++ R3 coversor de distancia.py
@@ -1,19 +1,24 @@
 #Conversor de millas a kilometros y vicecersa
 
-opcion=int("Bienvenido al conversor, elija la operacion que quiere realizar:\n1-Millas a kilometros\n2-Kilometros a millas")
+opcion=int(input("Bienvenido al conversor, elija la operacion que quiere realizar:\n1-Millas a kilometros\n2-Kilometros a millas:\n"))
 
 while opcion>2 or opcion<1:
     print("Opcion incorrecta\nDebes elegir 1 si quieres convertir millas a kilometros\nDebes elegir 2 si quieres convertir kilometros a millas")
-
+    opcion=int(input("Elija la operacion que quiere realizar:\n1-Millas a kilometros\n2-Kilometros a millas:\n"))
 
 def milla_a_km():
-    return milla*1.609344
+    milla=float(input("Ingrese cantidad de millas: "))
+    kilometros=milla*1.609344
+    print(f"{milla} millas equivale a {kilometros} kilometros")
 
 def km_a_milla():
-    return km/1.609344
+    km=float(input("Ingrese cantidad de kilometros: "))
+    millas= km/1.609344
+    print(f"{km} kilometros equivale a {millas} millas")
+
 
 if opcion==1:
-    milla_a_km() 
+    milla_a_km()
 elif opcion==2:
     km_a_milla()
 
